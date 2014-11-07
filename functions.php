0cd11b6d38103606eaa88fa07cc970c1dd56753b
Jose Miguel Urdaneta <ch3jose@users.noreply.github.com>
2014-11-07 10:38:40 -0430

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,3 @@
+// Call the libraries
+require_once( trailingslashit( get_template_directory() ) . 'inc/pagination.php');
+require_once( trailingslashit( get_template_directory() ) . 'inc/utilities.php');
