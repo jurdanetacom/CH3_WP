31eb6074c0b7bd54913d45d2f0bd1b602cfe6092
Jose Miguel Urdaneta <jose@ch3.es>
2014-11-07 11:22:37 -0430

    Functions and files added
    Functions and files added to the repo, still under development.

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
-// Call the libraries
-require_once( trailingslashit( get_template_directory() ) . 'inc/pagination.php');
-require_once( trailingslashit( get_template_directory() ) . 'inc/utilities.php');
+<?php
+//*************************
+//*****HOW TO USE**********
+//*************************
+
+// Call the libraries, simply copy and paste the content of this repo in your theme folder, and copy this lines in your functions.php file
+
+require_once( trailingslashit( get_template_directory() ) . 'inc/ch3/pagination.php');
+require_once( trailingslashit( get_template_directory() ) . 'inc/ch3/utilities.php');
+require_once( trailingslashit( get_template_directory() ) . 'inc/ch3/branding.php');
+
+?>
